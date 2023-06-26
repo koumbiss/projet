a75c66452e9b0c1613ecf7559cce7b8390c34a44
koumbiss <koumbskoita@gmail.com>
2023-06-27 00:33:59 +0100

    b commit

--- lib/model/Collection.dart
+++ lib/model/Collection.dart
@@ -3,24 +3,24 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Collection {
   int idCollection;
   String nomCollection, image;
-  DateTime Date;
+  DateTime date;
 
   Collection(
       {required this.idCollection,
       required this.nomCollection,
       required this.image,
-      required this.Date});
+      required this.date});
 
   static Collection fromJson(Map<String, dynamic> json) => Collection(
         idCollection: json['idCollection'],
         nomCollection: json['nomCollection'],
         image: json['image'],
-        Date: (json['Date'] as Timestamp).toDate(),
+        date: (json['date'] as Timestamp).toDate(),
       );
   Map<String, dynamic> toJson() => {
         'idCollection': idCollection,
         'nomCollection': nomCollection,
         'image': image,
-        'Date': Date,
+        'date': date,
       };
 }
